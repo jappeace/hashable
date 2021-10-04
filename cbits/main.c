8ba4676e4630f439fed7345c3c5e0353b33a3c16
Jappie Klooster <jappieklooster@hotmail.com>
2021-10-04 22:01:00 +0200

    show how it breaks

--- cbits/main.c
+++ cbits/main.c
@@ -9,12 +9,13 @@ int main () {
   printf("\n hello world \n");
   uint64_t v[4] = {0,0,0,0};
   hashable_siphash_init(4,5,v);
-  for(int i = 0; i < 4; i++) {
-      printf("%i, %ld \n", i, v[i]);
-  }
-  hashable_siphash_compression(1,v,"a",0,1);
-  printf("\n after compression \n ");
-  for(int i = 0; i < 4; i++) {
-      printf("%i, %ld \n", i, v[i]);
-  }
+  hashable_siphash_compression(1,v,"AB",0,2);
+  uint64_t res2 = hashable_siphash_finalize(4, v);
+
+  hashable_siphash_init(4,5,v);
+  hashable_siphash_compression(1,v,"A",0,1);
+  hashable_siphash_compression(1,v,"B",0,1);
+  uint64_t res1 = hashable_siphash_finalize(4, v);
+
+  printf("%d == %d D", res2, res1);
 };
